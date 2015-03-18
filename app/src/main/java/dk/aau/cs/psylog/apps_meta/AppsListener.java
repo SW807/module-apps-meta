6f142c007df2de6f05b9be9a279a3c4ce3a7f5a0
Bruno Thalmann <mr.thalmann@gmail.com>
2015-03-18 10:46:49 +0100

    added some code commented out that can be used if we choose to change minsdk to 21

--- app/src/main/java/dk/aau/cs/psylog/apps_meta/AppsListener.java
+++ app/src/main/java/dk/aau/cs/psylog/apps_meta/AppsListener.java
@@ -1,11 +1,15 @@
 package dk.aau.cs.psylog.apps_meta;
 
+import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+
+import java.util.Calendar;
+import java.util.List;
 
 import dk.aau.cs.psylog.module_lib.ISensor;
 
@@ -22,7 +26,19 @@ public class AppsListener implements SensorEventListener, ISensor {
         {
 
             //UsageStatsManager usageStatsManager = new UsageStatsManager(context, context.getSystemService("USAGE_STATS_SERVICE"));
-            
+            //context.getSystemService(Context.USAGE_STATS_SERVICE);
+            //final UsageStatsManager usageStatsManager=(UsageStatsManager)context.getSystemService(Context.USAGE_STATS_SERVICE);
+            /*final Calendar beginCal=Calendar.getInstance();
+            beginCal.set(Calendar.DATE,1);
+            beginCal.set(Calendar.MONTH,0);
+            beginCal.set(Calendar.YEAR,2012);
+            final Calendar endCal=Calendar.getInstance();
+            endCal.set(Calendar.DATE,1);
+            endCal.set(Calendar.MONTH,0);
+            endCal.set(Calendar.YEAR,2014);
+            final UsageStatsManager usageStatsManager=(UsageStatsManager)context.getSystemService("usagestats");// Context.USAGE_STATS_SERVICE);
+            final List<UsageStats> queryUsageStats=usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_YEARLY,beginCal.getTimeInMillis(),endCal.getTimeInMillis());
+            */
 
         }
 
